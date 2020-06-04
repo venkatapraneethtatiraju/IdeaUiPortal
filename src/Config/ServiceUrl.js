4d3f5514a70f9171309ace8fe0f00444daf72201
Bikrant Singh <Bikrant.Singh@XEBIAINDIA.LOCAL>
2020-06-04 17:23:54 +0530

    Change Recently submitted idea style

--- src/Config/ServiceUrl.js
+++ src/Config/ServiceUrl.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://iportal.herokuapp.com/innovation-portal/api/v1';
+export const BASE_URL = 'https://cors-anywhere.herokuapp.com/http://iportal.herokuapp.com/innovation-portal/api/v1';
 export const TOPTRENDING_IDEAS_URL = BASE_URL + '/dashboard/toptrending';
 export const TOPCONTRIBUTORS_URL = BASE_URL + '/dashboard/topcontributor';
 export const IDEASTATS_URL = BASE_URL + '/dashboard/ideastats/';
--- src/components/Dashboard/RecentlySubmittedIdeas/RecentlySubmittedIdeas.js
+++ src/components/Dashboard/RecentlySubmittedIdeas/RecentlySubmittedIdeas.js
@@ -1,153 +1,149 @@
 import React, { Component } from 'react';
 import "./RecentlySubmittedIdeas.scss";
-import { Col, Card, Row, Table} from 'antd';
+import { Col, Table } from 'antd';
 import { getToken } from '../../Auth/Auth';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Axios from '../../Axios/Axios';
 import PopUpModel from '../../PopUpModel/PopUpModel'
-import {getIdeaDetailsById } from '../../../services/AppService';
+import { getIdeaDetailsById } from '../../../services/AppService';
 
 class RecentlySubmittedIdeas extends Component {
-  
+
     constructor(props) {
         super(props);
         this.state = {
-            pagination : false,
-            size : '4',
-            ideaData : [],
-            cardName : 'Recently Submitted Ideas',
-            showViewAll : false,
-            selectedRow : [],
+            pagination: false,
+            size: '4',
+            ideaData: [],
+            cardName: 'Recently Submitted Ideas',
+            showViewAll: false,
+            selectedRow: [],
             columns: [
                 {
-                  title: 'Idea Subject',
-                  dataIndex: 'ideaSubject',
-                 width : '35%',
+                    title: 'Idea Subject',
+                    dataIndex: 'ideaSubject',
+                    width: '35%',
+                    ellipsis: true,
                 },
                 {
-                  title: 'Submitted by',
-                  dataIndex: 'submittedBy',
-                  width : '25%',
+                    title: 'Submitted by',
+                    dataIndex: 'submittedBy',
+                    width: '25%',
+                    ellipsis: true,
                 },
                 {
-                  title: 'Submitted on',
-                  dataIndex: 'submittedOn',
-                  width : '20%',
-                 
+                    title: 'Submitted on',
+                    dataIndex: 'submittedOn',
+                    width: '20%',
+                    ellipsis: true,
                 },
                 {
                     title: 'Idea Category',
                     dataIndex: 'ideaCategory',
-                    width : '20%',
-                    
+                    width: '20%',
+                    ellipsis: true,
                 }
-               
             ]
         }
-      
     }
-    componentDidMount() {
-         this.getRecentIdeaDetails();       
-     }
 
-     getRecentIdeaDetails () {
-     const token = getToken();
-      Axios.get('/ideas/recent',{
+    componentDidMount() {
+        this.getRecentIdeaDetails();
+    }
+
+    getRecentIdeaDetails() {
+        const token = getToken();
+        Axios.get('/ideas/recent', {
             headers: {
                 Authorization: `Bearer ${token}`,
-              },
+            },
         })
-        .then(response => { 
-            console.log('getRecentIdeaDetails', response)
-            if(response.data.message === 'success'){
-
-                this.setItemItem(response.data.result);
-            }})
+            .then(response => {
+                console.log('getRecentIdeaDetails', response)
+                if (response.data.message === 'success') {
+                    this.setItemItem(response.data.result);
+                }
+            })
             .catch(error => {
             });
-        }
-            setItemItem = (result) => {
-            let newArr = result.map((val, index)=>{
-                return {
+    }
+
+    setItemItem = (result) => {
+        let newArr = result.map((val, index) => {
+            return {
                 key: val.id,
                 index: index,
                 ideaSubject: val.title ? val.title : "- ",
                 ideaType: val.categoryName ? val.categoryName : "-",
                 submittedBy: val.submittedBy ? val.submittedBy : "-",
                 submittedOn: val.submissionDate ? val.submissionDate : "-",
-                ideaCategory : val.categoryName ? val.categoryName : "-",
-                ideaDescription : val.ideaDescription ? val.ideaDescription :"-"
-                  
-                };
-            })
-            this.setState({ideaData : newArr,showViewAll : true});
+                ideaCategory: val.categoryName ? val.categoryName : "-",
+                ideaDescription: val.ideaDescription ? val.ideaDescription : "-"
+            };
+        })
+        this.setState({ ideaData: newArr, showViewAll: true });
+    }
 
-        }
+    buttonActionHandler = (event) => {
+        this.setState(prevstate => ({
+            ...prevstate,
+            showModal: !prevstate.showModal
+        }))
+    }
 
-        buttonActionHandler = (event) => {
-            this.setState(prevstate => ({
-              ...prevstate,
-              showModal: !prevstate.showModal
-            }))
-          }
+    onSelectedRowAction = (record, rowIndex) => {
+        if (record) {
+            this.getAllIdeaDetailsById(record.key)
+        }
+    }
 
-          onSelectedRowAction = (record, rowIndex) => {
-            if(record) {
-                this.getAllIdeaDetailsById(record.key)
-            }
+    getAllIdeaDetailsById(ideaId) {
+        if (ideaId) {
+            getIdeaDetailsById(ideaId)
+                .then(response => {
+                    console.log('getAllIdeaDetails', response)
+                    if (response.data.message === 'success') {
+                        this.setState({ selectedRow: response.data.result, showModal: true })
+                    }
+                })
+                .catch(error => {
+                });
         }
-    
-        getAllIdeaDetailsById(ideaId) {
-                if(ideaId){
-                    getIdeaDetailsById(ideaId)
-                    .then(response => {
-                        console.log('getAllIdeaDetails', response)
-                        if (response.data.message === 'success') {
-                            this.setState({selectedRow : response.data.result,showModal : true})
-                        }
-                    })
-                    .catch(error => {
-                    });
-                   }
-        }
-    
+    }
+
     render() {
 
-        var topRecords =[];
-        if(this.state.ideaData.length>0) {
-            topRecords= this.state.ideaData.slice(0,5);
+        var topRecords = [];
+        if (this.state.ideaData.length > 0) {
+            topRecords = this.state.ideaData.slice(0, 5);
         }
 
 
         return (
-            <div className="recentSubmitted-container">
-              
-                <Col>
-                    <h2>{this.state.cardName}</h2>
-                    <Table    
-                    {...this.state}              
-                    columns={this.state.columns} 
+            <Col span={15} className="recentSubmitted-container">
+                <h2>{this.state.cardName}</h2>
+                <Table
+                    {...this.state}
+                    columns={this.state.columns}
                     dataSource={topRecords}
-                    pagination={this.state.pagination} 
+                    pagination={this.state.pagination}
                     onRow={(record, rowIndex) => ({
                         onClick: () => this.onSelectedRowAction(record, rowIndex)
-                        })}
-                    >
-                    </Table>
-                    {this.state.showViewAll && 
-                        <Link className="viewData" to='/allIdeas'  
-                    visible={this.state.showViewAll}>View all ideas</Link>  }
-                </Col>
+                    })}
+                >
+                </Table>
+                {this.state.showViewAll &&
+                    <Link className="viewData" to='/allIdeas'
+                        visible={this.state.showViewAll}>View all ideas</Link>}
 
                 {this.state.showModal ? <PopUpModel
-             onOk={this.buttonActionHandler}
-             onCancel={this.buttonActionHandler}
-             selectedRow ={this.state.selectedRow}  
-             isAddEditIdea="false"
-             isViewIdea="true"     
-            /> : null}
-            
-             </div>
+                    onOk={this.buttonActionHandler}
+                    onCancel={this.buttonActionHandler}
+                    selectedRow={this.state.selectedRow}
+                    isAddEditIdea="false"
+                    isViewIdea="true"
+                /> : null}
+            </Col>
         );
     }
 }
